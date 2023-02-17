4c89d0b12de73a9df2ce0290405646554083c01c
= <=>
2023-02-17 11:32:18 -0800

    print by printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - print a string
+ * Description: with proper grammar, but the outcome is a piece of art,
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); }
