ecd8291af0655b9545b407f9019d5ea675eb45cc
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-02-20 20:55:50 +0200

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,15 @@
-#include <unistd.h> 
-#include <sys/syscall.h> 
-int main(void) 
-{ 
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+
+int main(void)
+{
     const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, 1, msg, sizeof(msg)); 
-    return (1); 
+    syscall(SYS_write, 1, msg, sizeof(msg));
+    return (1);
 }
