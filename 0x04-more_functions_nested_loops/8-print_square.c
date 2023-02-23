2fb8051325e442a0a374d8533b4a1f5226972d22
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-02-23 22:54:32 +0200

    task 8 - print_square with hashtags

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * 
+ *  print_square - print squares with hashtags
+ * @size: integer variable
+ * Return: void
 */
 void print_square(int size)
 {
 int i, j;
 for (i = 0; i < size; i++)
 {
-    if (size == 0 || size < 0 )
-    {
-        _putchar ('\n');
-        break;
-    }
-    for (j = 0; j< size; j++)
-        _putchar ('#');
-    _putchar ('\n');
+if (size == 0 || size < 0)
+{
+_putchar ('\n');
+break;
 }
-
+for (j = 0; j < size; j++)
+{_putchar ('#');
+}
+_putchar ('\n');
+}
 }
