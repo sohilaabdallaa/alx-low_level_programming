5d2482b7368a2d8cc239721c80852cca1f4fd6cc
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-02-23 19:11:24 +0200

    task 3 - multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Entry point of program
+ * Description: multiplies two numbers
+ * @a: integer variable
+ * @b: integer variable
+ * Return: result of multipilcation
+ */
+int mul(int a, int b)
+{return (a * b);
+}
