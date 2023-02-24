b851f3e17b4ff7a5af406abf99862afc839be3bd
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-02-24 19:51:40 +0200

    update

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -7,6 +7,15 @@
  */
 int main(void)
 {
-    print_numbers();
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
     return (0);
 }
