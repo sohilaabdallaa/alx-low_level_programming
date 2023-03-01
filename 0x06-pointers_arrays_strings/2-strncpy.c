38e1b9d67556cd68d802f972e3f626be1aeea1c4
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-03-01 22:07:44 +0200

    task 2 - strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,21 +10,16 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
 int s = 0;
-int j = 0;
 
 while (src[s] != '\0')
 s++;
-if (n > s)
-n = s;
-while (n--)
+
+for (s = 0; s < n && src[i]; s++)
 {
-dest[i] = src[j];
+dest[i] = src[i];
 i++;
-j++;
-if ( src[j] == '\0')
-{
-dest[i++] = '\0';
 }
-}
+for (i = s; i < n; i++)
+dest[i] = '\0';
 return (dest);
 }
