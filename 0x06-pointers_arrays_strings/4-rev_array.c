0f5198d5d59fa037675f87e32d038b1ad19526ab
= <=>
2023-03-02 03:09:36 -0800

    task 4 - reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * reverse_array - reverse numbers of integer array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+int i = 0;
+int tmp;
+int *ptr;
+ptr = &(a[n - 1]);
+while (n >= i)
+{
+tmp = a[i];
+a[i] = *(ptr);
+*ptr = tmp;
+i++;
+ptr--;
+n--;
+}
+}
+
