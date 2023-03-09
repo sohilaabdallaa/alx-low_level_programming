702309b6d7f7472066bcfe4c5f7a5908e6e041e6
= <=>
2023-03-09 01:17:53 -0800

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,17 @@
-#include <main.h>
+#include "main.h"
 /**
- * put_recursion: print string followed by newline
+ * _puts_recursion -  print string followed by newline
  * @s: pointer to char
  * Return: void
- * */
+ */
 void _puts_recursion(char *s)
 {
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
+
