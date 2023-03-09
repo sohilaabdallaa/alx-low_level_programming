4969e08d3f30b8f6f0cb7f1173792962258f63ca
= <=>
2023-03-09 03:13:06 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - count length of string.
+ * @s:string.
+ * Return: int length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
