027a76fe6ad4ea7d34ed188bb79972bf943850f0
= <=>
2023-03-09 03:42:02 -0800

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_pow_recursion - calculate x power y.
+ *@x: intger base number.
+ *@y: integer power number.
+ *Return: int result.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 0)
+		return (0);
+	if (y == 0)
+		return (x);
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y + 1));
+}
+
