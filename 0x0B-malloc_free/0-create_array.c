aa8b9d1707dc9cf717477a2b517ec6db72c0d71b
= <=>
2023-03-13 04:22:06 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - dynamically allocate array of size n.
+ * @size: integer define size of array.
+ * @c: char used to initialize array with.
+ * Return:IF size = 0 NULL , else Pointer to start of the array.
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr = NULL;
+	unsigned int i = 0;
+
+	if (size > 0)
+	{
+		ptr = (char *) malloc(size);
+		for (i = 0; i < size; i++)
+			ptr[i] = c;
+	}
+	return (ptr);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main.h Header file that contains all prototypes of functions 
+ * 			used in 0x0B-malloc_free projects */
+char *create_array(unsigned int size, char c);
+int _putchar(char c);
+#endif
