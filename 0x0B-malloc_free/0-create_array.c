63026472b0bbf3c15eecf4e2f91b8067bc7a1812
= <=>
2023-03-13 04:25:27 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,8 +15,11 @@ char *create_array(unsigned int size, char c)
 	if (size > 0)
 	{
 		ptr = (char *) malloc(size);
-		for (i = 0; i < size; i++)
-			ptr[i] = c;
+		if(ptr)
+		{
+			for (i = 0; i < size; i++)
+				ptr[i] = c;
+		}
 	}
 	return (ptr);
 }
