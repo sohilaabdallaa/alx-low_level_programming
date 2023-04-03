699f046fbe0c3d0ea7d261ee4dbe9f572becfa08
sohilaabdalla1laa <sohilaabdalla113@gmail.com>
2023-04-03 12:43:49 -0700

    Done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,14 +8,13 @@
  * Description: singly linked list node structure
  *
  */
-
-#include <stdlib.h>
-
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
+
+#include <stdlib.h>
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
