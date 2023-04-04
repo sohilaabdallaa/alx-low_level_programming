662de7b59b331c394fcf7c89ea7786d370a3197e
sohilaabdalla1laa <sohilaabdalla113@gmail.com>
2023-04-04 16:07:07 -0700

    Done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,15 +4,15 @@
  * insert_nodeint_at_index -  inserts a new node at a given position.
  * @head : pointer to a pointer to head of list.
  * @idx : index where new node will be added, Index starts at 0.
- *@n : integer data of new node.
+ * @n : integer data of new node.
  * Return:the address of the new node, or NULL if it failed.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new, *temp;
-	unsigned int counter = 0;
-	temp = *head;
+	unsigned int counter;
 
+	temp = *head;
 	new = malloc(sizeof(listint_t));
 	new->n = n;
 	new->next = NULL;
@@ -20,11 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = new;
-		return(new);
+		return (new);
 	}
-	/*check if the index idx is out of bounds, 
-	 * i.e., greater than the number of nodes in the list*/
-	for (counter = 0; counter <idx - 1; counter++)
+	/**
+	 * check if the index idx is out of bounds,
+	 * i.e., greater than the number of nodes in the list
+	 */
+	for (counter = 0; counter < idx - 1; counter++)
 	{
 		temp = temp->next;
 		if (temp == NULL)
