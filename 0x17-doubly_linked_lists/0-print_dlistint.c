de395367d69f505fa7b6bb4e247aee6782cf3c8d
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-15 02:27:01 -0700

    print

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -12,9 +12,9 @@ size_t print_dlistint(const dlistint_t *h)
 		return (0);
 	while (h)
 	{
-		number_nodes++;
 		printf("%d \n", h->n);
 		h = h->next;
+		number_nodes++;
 	}
 	return (number_nodes);
 }
