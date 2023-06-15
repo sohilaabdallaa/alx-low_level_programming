872d200edff3a190115f0b4666cfe30919881cd0
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-15 02:41:49 -0700

    add at the beg

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,20 @@
+/**
+ * add_dnodeint - adds a new node at the beginning of a list
+ * @head: double pointer to list.
+ * @n: intger value to be added
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL;
+
+	if (*head == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head->prev = new_node;
+	new_node->prev = NULL;
+	return (new_node);
+
+}
