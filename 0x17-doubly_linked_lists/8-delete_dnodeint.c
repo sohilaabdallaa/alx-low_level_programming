f6cb686324e2b4ccc1a688a8ec07475d0e83cfc9
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-06-16 15:07:01 -0700

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,7 +8,6 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *current = *head;
-	unsigned int i = 0;
 
 	if (!head)
 		return (-1);
@@ -21,16 +20,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-	for (i = 0; i < index && current; i++)
+	while (index && current)
 	{
 		current = current->next;
+		index--;
 	}
-	if (current == NULL || current->next == NULL)
+	if (index)
 		return (-1);
-	current->prev->next = current->next;
-	if (current->next)
-		current->next->prev = current->prev;
-	free(current);
-	return (1);
+
+	if (!index & current)
+	{
+		if (current->next)
+			current->next->prev = current->prev;
+		if (current->prev != NULL)
+			cursor->prev->next = current->next;
+		else
+			*head = current->next;
+		free(current);
+		return (1);
+	}
+	return (-1);
 
 }
