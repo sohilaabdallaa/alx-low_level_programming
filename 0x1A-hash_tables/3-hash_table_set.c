6b03ff8f2ff33181a581471745813636cf3c0eff
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-07 15:56:42 -0700

    set | update node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,6 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
+		free(hs_node);
 		ht->array[index]->value = strdup(value);
 		return (1);
 	}
