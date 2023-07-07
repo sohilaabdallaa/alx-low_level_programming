563f15a59f95e1cf286c7e594a9f81f95c898db4
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-07 16:28:51 -0700

    delete whole table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes the whole hash table.
+ * @ht:the hash table.
+ * Return:Void.
+**/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head = ht;
+	hash_node_t *hs_node, *tmp;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			hs_node = ht->array[i];
+			while (hs_node != NULL)
+			{
+				tmp = hs_node->next;
+				free(hs_node->key);
+				free(hs_node->value);
+				free(hs_node);
+				node = tmp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
