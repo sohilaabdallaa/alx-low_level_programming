b9849256efcc201781fd72f99eed1ae91f1b4696
sohilaabdallaa <sohilaabdalla113@gmail.com>
2023-07-07 16:05:31 -0700

    set | update node

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,20 +15,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || value == NULL)
 		return (0);
-/*	hs_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+
+	hs_node = (hash_node_t *) malloc(sizeof(hash_node_t));
 	if (!hs_node)
 		return (0);
-*/
+
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
+		free(hs_node);
 		return (1);
 	}
 
-        hs_node = (hash_node_t *) malloc(sizeof(hash_node_t));
-        if (!hs_node)
-                return (0);
 	hs_node->key = strdup(key);
 	hs_node->value = strdup(value);
 	hs_node->next = ht->array[index];
